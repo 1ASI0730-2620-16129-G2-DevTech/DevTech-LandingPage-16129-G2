97978c356b8ae841911270935cfd2428cda489ff
Arianapv082 <arianaperez81106@gmail.com>
2026-09-07 19:24:58 -0500

    refactor: update index.html to align with rubric requirements and team branding

--- menu_toggle.js
+++ menu_toggle.js
@@ -4,12 +4,7 @@ const btn = document.querySelector(".menu-toggle");
 // Selecciona el menú horizontal
 const menu = document.querySelector(".Menu-Horizontal");
 
-// Al hacer clic en el botón de hamburguesa
 btn.addEventListener("click", () => {
-  // Alterna (agrega o quita) la clase "active" en el menú
-  // Esto hace que se muestre o se oculte
   menu.classList.toggle("active");
 
-  // (opcional) También puedes alternar una clase en el botón si quieres cambiar su apariencia
-  // btn.classList.toggle("open");
 });
